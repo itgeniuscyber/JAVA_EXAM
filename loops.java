03d7c1a8197ceaf10d3cedd82e9a60b3c50f6890
Nelly Twaha <53542212+itgeniuscyber@users.noreply.github.com>
2025-07-05 14:05:22 +0300

    Create loops.java
    3c

---
+++ loops.java
@@ -0,0 +1,10 @@
+//Example using while loop
+int day = 1;
+while (day <= 30) {
+    System.out.println("Tracking PM2.5 for day: " + day);
+    day++;
+}
+//Example using For loop
+for (int day = 1; day <= 30; day++) {
+    System.out.println("Tracking PM2.5 for day: " + day);
+}
